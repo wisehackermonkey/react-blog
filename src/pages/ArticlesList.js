7084fb60c1be614ab7c8cf557a21563202cb942f
Oran C <oranbusiness@gmail.com>
2021-11-02 20:58:17 -0700

    [add] article list page

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,9 +1,19 @@
 import React from "react";
-
+import articlesContent from "./article-content"
+import { Link } from "react-router-dom";
 const ArticlesList = () => {
+    const articles = articlesContent
     return (
         <>
             <h1>Articles</h1>
+
+            {
+                articles.map((artile, key) => (
+                    <Link className="article-list-item" key={key} to={`/article/${artile.name}`}>
+                        <h3  >{artile.title}</h3>
+                    </Link>
+                ))
+            }
 
         </>
     )
